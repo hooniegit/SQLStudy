a8087abd44393a7b2c57c3c036146be80c401566
hooniegit <neivekim76@gmail.com>
2024-04-14 19:27:45 +0900

    240414 feat: JOIN Query

---
+++ SELECT/JOIN.sql
@@ -0,0 +1,33 @@
+-- TableName: TableOne, TableTwo, TableThree
+-- Column: EventName, StartDate, EndDate
+
+SELECT
+    *
+FROM
+    (
+        SELECT
+            *
+        FROM
+            TableOne
+        INNER JOIN
+            TableTwo
+        ON
+            TableOne.ColumnOne = TableTwo.ColumnTwo
+        INNER JOIN
+            (
+                SELECT
+                    EventName,
+                    Explanation,
+                    StartDate,
+                    EndDate,
+                    DATEDIFF(MINUTE, StartDate, CASE WHEN EndDate IS NULL THEN GETDATE() ELSE EndDate END) AS Duration
+                FROM
+                    TableThree
+            ) AS TableFour
+        ON
+            TableTwo.Detail = REPLACE(TableFour.Explanation, 'EXP=(', ''), ')', '') 
+    ) As TableFive
+WHERE
+    Duration > 5
+ORDER BY
+    StartDate DESC;
